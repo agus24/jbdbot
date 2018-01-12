714278a47ef9ffdafc46a7be0fbba09adab080c4
agus24 <agusx244@gmail.com>
2018-01-13 00:02:58 +0700

    a

--- index.php
+++ index.php
@@ -7,4 +7,4 @@ use LINE\LINEBot;
 $httpClient = new CurlHTTPClient('1556985519');
 $bot = new LINEBot($httpClient, ['channelSecret' => 'c51ade180370b4a509f9029d22c1a5e1']);
 
-$response = $bot->replyText('hello', 'hello!');
+$response = $bot->replyText('+tShab+/Xdwua1X9VLKEbKkqnVyb20zqeUhH+7YcPQe5n9LqE8d9xO0zKoLcqXG2M6ClqxT12EQh3iHew2nNcer/G2nc4t2To9JrjYKFwQZvLw8R6AiMsYSWCNU5v/lk+wTUAIXi/XecfVt9FjxzyQdB04t89/1O/w1cDnyilFU=', 'hello!');

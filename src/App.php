e1770195f41dc8fbbcf9be12c4a53369d4980c3b
agus24 <agusx244@gmail.com>
2018-01-13 00:40:56 +0700

    A

--- src/App.php
+++ src/App.php
@@ -23,7 +23,7 @@ class App
     {
         $app = new static;
         $log = Log::instance();
-        $httpClient = new CurlHTTPClient('1556985519');
+        $httpClient = new CurlHTTPClient('+tShab+/Xdwua1X9VLKEbKkqnVyb20zqeUhH+7YcPQe5n9LqE8d9xO0zKoLcqXG2M6ClqxT12EQh3iHew2nNcer/G2nc4t2To9JrjYKFwQZvLw8R6AiMsYSWCNU5v/lk+wTUAIXi/XecfVt9FjxzyQdB04t89/1O/w1cDnyilFU=');
         $bot = new LINEBot($httpClient, ['channelSecret' => 'c51ade180370b4a509f9029d22c1a5e1']);
         $app->log->info(json_encode($_SERVER));
     }

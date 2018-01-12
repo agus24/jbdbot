67797ae0d2c15915f5a3f48cb72a65ffef67dc85
agus24 <agusx244@gmail.com>
2018-01-13 00:43:30 +0700

    fix

--- src/App.php
+++ src/App.php
@@ -25,6 +25,38 @@ class App
         $log = Log::instance();
         $httpClient = new CurlHTTPClient('+tShab+/Xdwua1X9VLKEbKkqnVyb20zqeUhH+7YcPQe5n9LqE8d9xO0zKoLcqXG2M6ClqxT12EQh3iHew2nNcer/G2nc4t2To9JrjYKFwQZvLw8R6AiMsYSWCNU5v/lk+wTUAIXi/XecfVt9FjxzyQdB04t89/1O/w1cDnyilFU=');
         $bot = new LINEBot($httpClient, ['channelSecret' => 'c51ade180370b4a509f9029d22c1a5e1']);
-        $app->log->info(json_encode($_SERVER));
+        $app->log->info('a');
+        $app->pushMessage("woi");
+    }
+
+    private function exec_url($fullurl,$channelAccessToken,$message)
+    {
+
+        $header = array(
+            "Content-Type: application/json",
+            'Authorization: Bearer '.$channelAccessToken,
+        );
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_POST,           1 );
+        curl_setopt($ch, CURLOPT_POSTFIELDS,     $message);
+        curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_URL, $fullurl);
+
+        $returned =  curl_exec($ch);
+
+        return($returned);
+    }
+
+    public function pushMessage($message)
+    {
+        $response = exec_url('https://api.line.me/v2/bot/message/push',$this->channelAccessToken,json_encode($message));
     }
 }
